962e0d9d38713bbb2e96156439d3f7c490cae73e
Gustavo Costa <80131099+Gustavo-190321@users.noreply.github.com>
2021-09-10 23:39:52 -0300

    Add files via upload

---
+++ Section 3/desafio2.js
@@ -0,0 +1,4 @@
+let PI = 3.14, raio = 10, total;
+total = PI * raio * raio;
+console.log(total);
+console.log(typeof total);
---
+++ Section 3/desafio3.js
@@ -0,0 +1,8 @@
+let a = 7, b = 94;
+let temp = a;
+a = b;
+b = temp;
+
+// outra maneira mais facil : [a,b] = [b,a]
+
+console.log(a,b);
