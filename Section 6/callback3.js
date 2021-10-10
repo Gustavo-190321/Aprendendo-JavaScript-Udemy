a1d391d88386e7107bd6f378d7ef4c76b41ccfc0
Gustavo Costa <gustavomurilo.costa@hotmail.com>
2021-10-10 18:38:01 -0300

    Update

--- Section 6/callback3.js
+++ Section 6/callback3.js
@@ -1,4 +1,4 @@
 //exemplo de callback no browser
-document.getElementsByTagName('body')[0].onclick = fuction(e){
+document.getElementsByTagName('body')[0].onclick = fuction(e);{
     console.log('o evento ocorreu!')
 }
